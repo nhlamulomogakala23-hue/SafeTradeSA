a05fba92611dbc3da77e8fffeff9022e234a48f1
nhlamulomogakala23 <nhlamulomogakala23@gmail.com>
2026-06-04 17:34:06 +0200

    added project pages

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="style.css">
     <!-- FontAwesome for clean e-commerce icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
 
